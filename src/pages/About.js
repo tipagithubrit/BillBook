b0aa6bd824a60b685cbd837327ba0e27cbad3051
Karan singh mehra <mehrakannu2001@gmail.com>
2023-11-20 21:54:49 +0530

    edits

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,11 +8,11 @@ export default function About() {
         <div className="max-width">
           <div className="overlay">
             <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center mb-3 text-slate-800 lg:mb-8">
-              About Invoicer
+              About
             </h1>
             <p className="text-slate-700 text-center text-xl lg:w-2/3 lg:mx-auto">
-              Invoicer is an Invoicing application designed and built by Thomas
-              Sankara, whose main purpose is to - as the name suggests - create
+            BillBook is an Invoicing application designed and built by Karan mehra & team,
+             whose main purpose is to - as the name suggests - create
               and send invoices to clients, because this is something I've
               struggled with before. I hope it helps you as well.
             </p>
@@ -23,7 +23,7 @@ export default function About() {
               with me.
             </p>
 
-            <div className="lg:max-w-2xl lg:mx-auto">
+            {/* <div className="lg:max-w-2xl lg:mx-auto">
               <h3 className="text-2xl text-slate-800 mt-10 mb-5">
                 Some of the known issues include:
               </h3>
@@ -44,7 +44,7 @@ export default function About() {
                   other than the homepage
                 </li>
               </ol>
-            </div>
+            </div> */}
 
             <ul className="md:flex items-center justify-center mt-10 xl:max-w-3xl xl:mx-auto">
               <li className="flex flex-col text-slate-700 mb-5 md:flex-row md:items-center md:mb-0 md:mr-5">
